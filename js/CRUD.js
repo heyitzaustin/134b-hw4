cf68283ab4fd840fd28aa2ed226d2df28c49c0be
Austin Li <heyitzaustin@gmail.com>
2015-11-12 17:09:36 -0800

    Mostly working List and working Delete

--- js/CRUD.js
+++ js/CRUD.js
@@ -36,22 +36,61 @@ function listHabits(){
 
     // TODO: retrieve habits and list them
 
+
     habitsRef.on("value", function(snapshot) {
-        console.log(snapshot.val());
+        snapshot.forEach(function(childSnapshot){
+            console.log(childSnapshot.val());
+            var newLi = document.createElement('li');
+            newLi.id = childSnapshot.key();
+            newLi.innerHTML = "<ul class=\"habit-info\">" +
+                "<li><div class=\"habit-name\">"+childSnapshot.val().title+"</div></li>" +
+                "<li><img class=\"habit-icon\" src=\"../img/"+childSnapshot.val().icon+"\" alt=\"habit icon\"></li> " +
+                "</ul> " +
+                "<div class=\"message\"> " +
+                "<span class=\"message-total\"> " +
+                "<strong>2</strong> days in a row! Best Record: <strong>5</strong><br> " +
+                "<svg height=\"25\" width=\"150\">"+
+                    "<line x1=\"0\" y1=\"0\" x2=\"60\" y2=\"0\" style=\"stroke:rgba(65, 131, 215, 0.8);stroke-width:25\" />"+
+                    "<line x1=\"60\" y1=\"0\" x2=\"150\" y2=\"0\" style=\"stroke:rgba(171,171,171,0.6);stroke-width:25\" />"+
+                "</svg>"+
+                "</span><br> " +
+                "<span class=\"message-today\">Completed <strong>1/1</strong> for today!</span>" +
+                "</div>"+
+                "<div class=\"habit-op\">"+
+                    "<button type=\"button\" class=\"op op-done\" onclick=\"showMsg(this);\" title=\"done\">"+
+                        "<img src=\"../img/done.svg\" alt=\"Done\">"+
+                    "</button>"+
+                    "<button type=\"button\" class=\"op op-edit\" onclick=\"location.href='edit.html'\" title=\"edit habit\">"+
+                        "<img src=\"../img/edit.svg\" alt=\"Edit\">"+
+                    "</button>"+
+                    "<button type=\"button\" class=\"op op-del\" onclick=\"deleteHabit(this);\" title=\"delete habit\">"+
+                        "<img src=\"../img/delete.svg\" alt=\"Del\">"+
+                    "</button>"+
+                "</div>"+
+                "</li>";
+
+            var element = document.getElementById("habit-list");
+            element.appendChild(newLi);
+
+        });
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
 
+
+
 }
 
-function deleteHabit(){
+function removeHabit(key){
+
+    console.log(key);
 
     var myFirebaseRef = new Firebase("https://torrid-fire-6209.firebaseio.com");
 
     var habitsRef = myFirebaseRef.child("habits");
 
     // TODO: Insert unique ID of habit object here for deletion
-    var deleteRef = habitsRef.child("");
+    var deleteRef = habitsRef.child(key);
 
     var onComplete = function(error) {
         if (error) {
